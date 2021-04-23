a99c5e3c4a82d7338c55bc63076db7e073af0f8b
STEIN197 <nil20122013@gmail.com>
2021-04-23 20:18:07 +0500

    Compulation produces type declarations

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,17 +2,20 @@ import * as gulp from "gulp";
 import * as gulpTypescript from "gulp-typescript";
 import * as fs from "fs";
 
-const OUT_DIR: string = "out";
+const DIR_OUT: string = "out";
+const DIR_TYPES: string = "types";
 
 export default gulp.series(compile);
 
 export async function compile(): Promise<void> {
-	let project: gulpTypescript.Project = gulpTypescript.createProject("tsconfig.json");
-	project.src().pipe(project()).js.pipe(gulp.dest(OUT_DIR));
+	let project = gulpTypescript.createProject("tsconfig.json");
+	let result = project.src().pipe(project());
+	result.pipe(gulp.dest(DIR_TYPES));
+	result.js.pipe(gulp.dest(DIR_OUT));
 }
 
 export async function clean(): Promise<void> {
-	for (let dir of [OUT_DIR, "types"]) {
+	for (let dir of [DIR_OUT, DIR_TYPES]) {
 		fs.rmdirSync(dir, {
 			recursive: true
 		});
