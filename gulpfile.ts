ca8770567b8b599ae66eb0c941248c0bd016cc1c
STEIN197 <nil20122013@gmail.com>
2021-04-23 20:33:03 +0500

    Options are read directly from tsconfig

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,13 +2,22 @@ import * as gulp from "gulp";
 import * as gulpTypescript from "gulp-typescript";
 import * as fs from "fs";
 
-const DIR_OUT: string = "out";
-const DIR_TYPES: string = "types";
+const TSCONFIG_JSON = "tsconfig.json";
+let tsConfig: any;
+try {
+	tsConfig = JSON.parse(fs.readFileSync(TSCONFIG_JSON).toString())
+} catch (ex) {
+	console.log(ex.message);
+	process.exit();
+}
+
+const DIR_OUT: string = tsConfig.compilerOptions.outDir;
+const DIR_TYPES: string = tsConfig.compilerOptions.declarationDir;
 
 export default gulp.series(compile);
 
 export async function compile(): Promise<void> {
-	let project = gulpTypescript.createProject("tsconfig.json");
+	let project = gulpTypescript.createProject(TSCONFIG_JSON);
 	let result = project.src().pipe(project());
 	result.pipe(gulp.dest(DIR_TYPES));
 	result.js.pipe(gulp.dest(DIR_OUT));
