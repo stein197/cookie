992350f0e7e9d9aeb00345827426bf7818d7e61e
Nail Gafarov <nil20122013@gmail.com>
2021-08-02 16:05:04 +0500

    import json config

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,20 +1,11 @@
-import fs from "fs";
 import gulp from "gulp";
 import gulpTypescript from "gulp-typescript";
 import gulpUglify from "gulp-uglify";
 import webpackStream from "webpack-stream";
 import webpackConfig from "./webpack.config.js";
+import tsConfig from "./tsconfig.json";
 
 const TSCONFIG_JSON: string = "tsconfig.json";
-let tsConfig: any;
-
-try {
-	tsConfig = JSON.parse(fs.readFileSync(TSCONFIG_JSON).toString())
-} catch (ex) {
-	console.log(ex.message);
-	process.exit(1);
-}
-
 const TS_PROJECT: gulpTypescript.Project = gulpTypescript.createProject(TSCONFIG_JSON);
 const DIR_OUT: string = tsConfig.compilerOptions.outDir;
 
