bbaca0e05b4aeec902a78b93d4364245addd7888
Nail Gafarov <nil20122013@gmail.com>
2021-08-02 16:31:28 +0500

    Added babel

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,9 +2,11 @@ import fs from "fs";
 import gulp from "gulp";
 import gulpTypescript from "gulp-typescript";
 import gulpUglify from "gulp-uglify";
+import gulpBabel from "gulp-babel";
 import webpackStream from "webpack-stream";
 import webpackConfig from "./webpack.config.js";
 import tsConfig from "./tsconfig.json";
+import babelConfig from "./babel.config.json";
 
 const TSCONFIG_JSON: string = "tsconfig.json";
 const TS_PROJECT: gulpTypescript.Project = gulpTypescript.createProject(TSCONFIG_JSON);
@@ -14,7 +16,7 @@ const DIR_TYPES: string = tsConfig.compilerOptions.declarationDir;
 export default gulp.series(clean, build);
 
 export async function build(): Promise<void> {
-	TS_PROJECT.src().pipe(TS_PROJECT()).js.pipe(gulp.dest(DIR_OUT)).pipe(webpackStream(webpackConfig)).pipe(gulpUglify()).pipe(gulp.dest(DIR_OUT));
+	TS_PROJECT.src().pipe(TS_PROJECT()).js.pipe(gulp.dest(DIR_OUT)).pipe(gulpBabel(babelConfig)).pipe(gulp.dest(DIR_OUT)).pipe(webpackStream(webpackConfig)).pipe(gulpUglify()).pipe(gulp.dest(DIR_OUT)).pipe(gulp.dest(DIR_OUT));
 }
 
 export async function clean(): Promise<void> {
