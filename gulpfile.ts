de9f125edac1b3feae36cb82c8a08ad783f1781e
Nail Gafarov <nil20122013@gmail.com>
2021-08-02 16:14:41 +0500

    +function(clean())

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import gulp from "gulp";
 import gulpTypescript from "gulp-typescript";
 import gulpUglify from "gulp-uglify";
@@ -8,9 +9,18 @@ import tsConfig from "./tsconfig.json";
 const TSCONFIG_JSON: string = "tsconfig.json";
 const TS_PROJECT: gulpTypescript.Project = gulpTypescript.createProject(TSCONFIG_JSON);
 const DIR_OUT: string = tsConfig.compilerOptions.outDir;
+const DIR_TYPES: string = tsConfig.compilerOptions.declarationDir;
 
 export default build;
 
 export async function build(): Promise<NodeJS.ReadWriteStream> {
 	return TS_PROJECT.src().pipe(TS_PROJECT()).js.pipe(gulpUglify()).pipe(webpackStream(webpackConfig)).pipe(gulp.dest(DIR_OUT));
+}
+
+export async function clean(): Promise<void> {
+	for (const dir of [DIR_OUT, DIR_TYPES]) {
+		fs.rmdirSync(dir, {
+			recursive: true
+		});
+	}
 }
