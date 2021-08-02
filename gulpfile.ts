277c7190b8b1e370faac72be09113288e1a1ba0f
Nail Gafarov <nil20122013@gmail.com>
2021-08-02 13:37:51 +0500

    +devDependency(gulp-uglify)

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,6 +1,7 @@
-import * as gulp from "gulp";
-import * as fs from "fs";
-import * as gulpTypescript from "gulp-typescript";
+import fs from "fs";
+import gulp from "gulp";
+import gulpTypescript from "gulp-typescript";
+import gulpUglify from "gulp-uglify";
 
 const TSCONFIG_JSON: string = "tsconfig.json";
 let tsConfig: any;
@@ -18,5 +19,5 @@ const DIR_OUT: string = tsConfig.compilerOptions.outDir;
 export default compile;
 
 export async function compile(): Promise<NodeJS.ReadWriteStream> {
-	return TS_PROJECT.src().pipe(TS_PROJECT()).js.pipe(gulp.dest(DIR_OUT));
+	return TS_PROJECT.src().pipe(TS_PROJECT()).js.pipe(gulpUglify()).pipe(gulp.dest(DIR_OUT));
 }
